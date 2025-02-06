ffb12f509c62466d51aa16f328870db83e3048c6
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-06 20:54:23 +0200

    add: about and animals page (draft)

---
+++ app/about/page.jsx
@@ -0,0 +1,3 @@
+export default function AboutPage() {
+  return <div style={{ height: "100dvh" }}>About</div>;
+}
---
+++ app/animals/page.jsx
@@ -0,0 +1,3 @@
+export default function AnimalsPage() {
+  return <div style={{ height: "200dvh" }}>Animals</div>;
+}
