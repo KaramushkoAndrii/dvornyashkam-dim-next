6fec43ae41cfc114179ca4c7ee7ba1e1cbf639ce
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-06 13:29:58 +0200

    add: sass package

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Instructions:
+  // https://nextjs.org/docs/app/building-your-application/styling/sass#customizing-sass-options
+  // https://sass-lang.com/documentation/at-rules/use/
+  sassOptions: {
+    additionalData: `
+            @use "./app/styles/vars.scss" as *;
+            @use "./app/styles/mixins.scss" as *;
+        `,
+  },
+};
 
 export default nextConfig;
