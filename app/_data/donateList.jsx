a8a8baebee8e28fad05e0432be5a0767833d23f0
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-06 20:57:26 +0200

    ref: donateList style

--- app/_data/donateList.jsx
+++ app/_data/donateList.jsx
@@ -1,19 +1,14 @@
-
-
-
 const DonateList = [
-    {
-        name: 'payPal',
-        logo: './images/paypalLogo.svg',
-        src: 'https://send.monobank.ua/jar/2NuiKeFtMi'
-
-    },
-    {
-        name: 'monobank',
-        logo: './images/monoLogo.svg',
-        src: 'https://send.monobank.ua/jar/2NuiKeFtMi'
-    }
-]
-
+  {
+    name: "payPal",
+    logo: "./images/paypalLogo.svg",
+    src: "https://send.monobank.ua/jar/2NuiKeFtMi",
+  },
+  {
+    name: "monobank",
+    logo: "./images/monoLogo.svg",
+    src: "https://send.monobank.ua/jar/2NuiKeFtMi",
+  },
+];
 
 export default DonateList;
