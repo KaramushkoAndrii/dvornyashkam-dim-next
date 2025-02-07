1d3cb8d3ad23958c122bbe6e8c1484fc10a8815a
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-07 19:06:24 +0200

    fix: contact page link

--- app/_data/navigationList.jsx
+++ app/_data/navigationList.jsx
@@ -1,9 +1,7 @@
-
-
 export const navigationList = [
-    {title: "navigation.about_us", href: "/about"},
-    {title: "navigation.animals", href: "/animals"},
-    {title: "navigation.advise", href: "/advise"},
-    {title: "navigation.help", href: "/help"},
-    {title: "navigation.contacts", href: "/contacts"},
-]
+  { title: "navigation.about_us", href: "/about" },
+  { title: "navigation.animals", href: "/animals" },
+  { title: "navigation.advise", href: "/advise" },
+  { title: "navigation.help", href: "/help" },
+  { title: "navigation.contacts", href: "/contact" },
+];
