ec946b441390c5dc7e26d970cd655a120b105bf2
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-07 21:46:00 +0200

    fix: images path

--- app/_data/donateList.jsx
+++ app/_data/donateList.jsx
@@ -1,12 +1,12 @@
 const DonateList = [
   {
     name: "payPal",
-    logo: "./images/paypalLogo.svg",
+    logo: "/images/paypalLogo.svg",
     src: "https://send.monobank.ua/jar/2NuiKeFtMi",
   },
   {
     name: "monobank",
-    logo: "./images/monoLogo.svg",
+    logo: "/images/monoLogo.svg",
     src: "https://send.monobank.ua/jar/2NuiKeFtMi",
   },
 ];
