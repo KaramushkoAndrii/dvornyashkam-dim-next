32d5f60f83a21c1a95f91ba149c20ca34de99df9
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-07 18:08:25 +0200

    fix: AnimalsCategoryPage

--- app/animals/[category]/page.jsx
+++ app/animals/[category]/page.jsx
@@ -4,14 +4,29 @@ import catsDB from "@/app/_data/catsDB";
 
 import "./page.scss";
 
-export default function AnimalsCategoryPage(props) {
-  const title = "Category Title";
-  const btnMoreTitle = "More Category Title";
+export default function AnimalsCategoryPage({ params }) {
+  const { t } = { t: (x) => x }; // useTranslation();
+
+  let title = "Category Title";
+  let btnMoreTitle = "More Category Items";
+
+  switch (params?.category) {
+    case "cats":
+      title = t("cat-page.title");
+      btnMoreTitle = t("buttons.more-cats");
+      break;
+
+    case "dogs":
+      title = t("dog-page.title");
+      btnMoreTitle = t("buttons.more-dogs");
+      break;
+
+    default:
+      break;
+  }
 
   return (
     <div className="animals-category-page">
-      {JSON.stringify(props)}
-
       <AnimalsCategory
         title={title}
         btnMoreTitle={btnMoreTitle}
