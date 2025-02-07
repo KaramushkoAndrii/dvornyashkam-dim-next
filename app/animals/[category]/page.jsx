0c4a75a87cb08096abc4443cd9217bf0b9429ab0
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-07 17:34:37 +0200

    add: AnimalsCategory page (with error yet)

---
+++ app/animals/[category]/page.jsx
@@ -0,0 +1,22 @@
+import AnimalsCategory from "@/app/_components/animalsCategory/AnimalsCategory";
+
+import catsDB from "../../data/catsDB";
+
+import "./page.scss";
+
+export default function AnimalsCategoryPage(props) {
+  const title = "Category Title";
+  const btnMoreTitle = "More Category Title";
+
+  return (
+    <div className="animals-category-page">
+      {JSON.stringify(props)}
+
+      <AnimalsCategory
+        title={title}
+        btnMoreTitle={btnMoreTitle}
+        data={catsDB}
+      />
+    </div>
+  );
+}
