0208bf987b9f4113cc9b7c06c42b22bffc70078a
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-07 16:04:34 +0200

    upd: a home page markup (with errors yet)

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,19 @@
-// import Image from "next/image";
+import HeroSection from "../../components/heroSection/HeroSection";
+import SearchSection from "../../components/searchSection/SearchSection";
+import AboutSection from "../../components/aboutSection/AboutSection";
+import OurAnimals from "../../components/ourAnimals/OurAnimals";
+import HelpSection from "../../components/helpSection/HelpSection";
+
 import "./page.scss";
 
 export default function HomePage() {
   return (
-    <div className="page">
-      <h1 className="page__title">Home Page!</h1>
-    </div>
+    <>
+      <HeroSection />
+      <SearchSection />
+      <AboutSection />
+      <OurAnimals />
+      <HelpSection btnText={"buttons.more"} />
+    </>
   );
 }
