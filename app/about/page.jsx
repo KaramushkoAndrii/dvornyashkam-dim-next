085e58fb9378b9ff349dc930c1af01aa3e7e2380
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-07 14:19:53 +0200

    upd: an about page markup (with errors yet)

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,3 +1,39 @@
+import { useTranslation } from "react-i18next";
+import { motion } from "motion/react";
+
+import { slideFromLeft, slideFromRight } from "../../components/Animations";
+import FigureContainer from "../../components/figureContainer/FigureContainer";
+
+import "./page.scss";
+
 export default function AboutPage() {
-  return <div style={{ height: "100dvh" }}>About</div>;
+  const { t } = useTranslation();
+
+  return (
+    <section className="about-page">
+      <h2 {...slideFromLeft} className="page__title">
+        {t(`${title}`)}
+      </h2>
+      <ul className="about-page__list">
+        {dataList.map((item, key) => {
+          const slideText = key % 2 === 0 ? slideFromRight : slideFromLeft;
+          const slideImg = key % 2 === 0 ? slideFromLeft : slideFromRight;
+
+          return (
+            <li key={key} className="about-page__item">
+              <FigureContainer
+                {...slideImg}
+                src={item.img}
+                alt={item.alt}
+                text={item.alt}
+              />
+              <motion.p {...slideText}>
+                {t(`about-us-page.${item.text}`)}
+              </motion.p>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
