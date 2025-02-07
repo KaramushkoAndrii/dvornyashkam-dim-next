b035667f7aa6eaab4cbda86a7372b69c3397c383
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-07 16:20:26 +0200

    upd: an animals page markup (with errors yet)

--- app/animals/page.jsx
+++ app/animals/page.jsx
@@ -1,3 +1,44 @@
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { motion } from "motion/react";
+
+import {
+  slideFromLeft,
+  slideFromRight,
+  slideFromBottom,
+} from "../../components/Animations";
+
+import "./animalsPage.scss";
+
 export default function AnimalsPage() {
-  return <div style={{ height: "200dvh" }}>Animals</div>;
+  const { t } = useTranslation();
+
+  return (
+    <section className="animals__section">
+      <div className="animals__header">
+        <motion.h2 {...slideFromBottom}>{t("animals-page.title")}</motion.h2>
+        <motion.p {...slideFromBottom}>
+          {t("animals-page.description")}
+        </motion.p>
+      </div>
+      <ul className="animals-content">
+        {animalList.map((animal, i) => {
+          const animation = i === 0 ? slideFromLeft : slideFromRight;
+
+          return (
+            <motion.li
+              {...animation}
+              key={animal.id}
+              className={`animals-content__${animal.id}`}
+            >
+              <Link to={`${animal.src}`}>
+                <img src={animal.imgSrc} alt={animal.id} />
+                <span>{t(`${animal.titleKey}`)}</span>
+              </Link>
+            </motion.li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
