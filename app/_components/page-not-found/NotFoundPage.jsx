5b5ccea6cacaa56ad9e9f03d62c9bd94a2d4fa05
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-10 15:28:16 +0200

    add: NotFoundPage component

---
+++ app/_components/page-not-found/NotFoundPage.jsx
@@ -0,0 +1,16 @@
+import { useTranslations } from "next-intl";
+
+import "./NotFoundPage.scss";
+
+export default function NotFoundPage() {
+  // TO DO: Need some styles
+  const t = useTranslations("NotFound");
+
+  return (
+    <div className="not-found-page">
+      <h1 className="not-found-page__title">
+        <b>404</b> - {t("title")}
+      </h1>
+    </div>
+  );
+}
