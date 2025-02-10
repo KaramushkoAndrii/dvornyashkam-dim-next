932cfb880da0906a0a238eef83c15b5fdc886136
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-10 15:59:15 +0200

    upd: imports usePathname()

--- app/_components/navigation/Navigation.jsx
+++ app/_components/navigation/Navigation.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useTranslations } from "next-intl";
-import { usePathname } from "next/navigation";
+import { usePathname } from "@/i18n/routing";
 import { Link } from "@/i18n/routing";
 
 import { FaArrowRightLong } from "react-icons/fa6";
