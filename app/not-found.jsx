f41607b7a09576c0060906a0bf7e73916b86cdc7
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-02-10 15:34:52 +0200

    add: RootNotFoundPage

---
+++ app/not-found.jsx
@@ -0,0 +1,18 @@
+// https://github.com/amannn/next-intl/blob/main/examples/example-app-router/src/app/not-found.tsx
+
+import { routing } from "@/i18n/routing";
+
+import BaseLayout from "@/app/_components/base/BaseLayout";
+import NotFoundPage from "@/app/_components/page-not-found/NotFoundPage";
+
+// This page renders when a route like `/unknown.txt` is requested.
+// In this case, the layout at `app/[locale]/layout.jsx` receives
+// an invalid value as the `[locale]` param and calls `notFound()`.
+
+export default function RootNotFoundPage() {
+  return (
+    <BaseLayout locale={routing.defaultLocale}>
+      <NotFoundPage />
+    </BaseLayout>
+  );
+}
