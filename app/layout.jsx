b2b1d83dac46d37e79cdc82a76a5537a747cebc4
Andii Karamushko <karamushko1997@gmail.com>
2025-02-14 18:57:02 +0200

    add typography.scss

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 // https://github.com/amannn/next-intl/blob/main/examples/example-app-router/src/app/layout.tsx
 import "../assets/styles/normalize.scss";
 import "../assets/styles/global.scss";
+import "../assets/styles/typography.scss";
 
 // Since we have a `not-found.jsx` page on the root, a layout file
 // is required, even if it's just passing children through.
