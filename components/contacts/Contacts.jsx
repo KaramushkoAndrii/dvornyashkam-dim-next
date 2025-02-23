bbaecfba9e20f85c141f9ef2e363eabd7c6689e4
Andii Karamushko <karamushko1997@gmail.com>
2025-02-23 18:56:21 +0200

    fixed donateSection

--- components/contacts/Contacts.jsx
+++ components/contacts/Contacts.jsx
@@ -7,9 +7,13 @@ import "./contacts.scss";
 const Contacts = () => {
   return (
     <div className="contacts">
-      <ul className="contacts__links">
-        <li className="contacts__links-item">
-          <a target="_blank" href={`mailto:${ContactsLinks.eamil}`}>
+      <ul className="contacts__list">
+        <li className="contacts__item">
+          <a
+            className="contacts__link"
+            target="_blank"
+            href={`mailto:${ContactsLinks.eamil}`}
+          >
             {ContactsLinks.eamil}
           </a>
         </li>
