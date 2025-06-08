0f839ec6cd6a9f225a2b9abecb8c5539e39f65aa
Andii Karamushko <karamushko1997@gmail.com>
2025-06-08 23:00:41 +0300

    ref: add shadows and scale at hover in all-animals__item

--- components/page-animals-category/allAnimalsList/AllAnimalsList.jsx
+++ components/page-animals-category/allAnimalsList/AllAnimalsList.jsx
@@ -1,15 +1,32 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 
 import AnimalCard from "@/components/common/AnimalCard/AnimalCard";
 
 import "./allAnimalsList.scss";
 
 const AllAnimalsList = ({ list }) => {
+  const [activeCard, setActiveCard] = useState(false);
+  const mainClass = "all-animals__item";
   return (
     <>
       <ul className="all-animals__list">
         {list.map((item) => (
-          <li key={item.id} className="all-animals__item">
+          <li
+            key={item.id}
+            // className={
+            //   activeCard === item.id
+            //     ? "all-animals__item--active"
+            //     : "all-animals__item"
+            // }
+
+            className={`${mainClass} ${
+              activeCard === item.id ? `${mainClass}--active` : ""
+            }`}
+            onMouseEnter={() => setActiveCard(item.id)}
+            onMouseLeave={() => setActiveCard(null)}
+            onTouchStart={() => setActiveCard(item.id)}
+            onTouchEnd={() => setActiveCard(null)}
+          >
             <AnimalCard animal={item} />
           </li>
         ))}
