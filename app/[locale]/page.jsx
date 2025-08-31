4424f4c59ee413eda075777d8ec4178a989133ad
Andii Karamushko <karamushko1997@gmail.com>
2025-08-31 22:43:47 +0300

    ref: created funtion fetchApi and use it in home page for get data from API

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -5,6 +5,7 @@ import SearchSection from "@/components/page-home/searchSection/SearchSection";
 import AboutSection from "@/components/page-home/aboutSection/AboutSection";
 import OurAnimals from "@/components/page-home/ourAnimals/OurAnimals";
 import HelpSection from "@/components/common/helpSection/HelpSection";
+import { fetchApi } from "@/lib/api";
 
 // import dogsDB from "@/data/dogsDB";
 // import catsDB from "@/data/catsDB";
@@ -18,61 +19,24 @@ import next from "next";
 export default async function HomePage() {
   const t = await getTranslations();
 
-  const heroData = await fetch("http://localhost:1337/api/hero-section", {
-    next: { revalidate: 60 },
-  })
-    .then((res) => {
-      if (!res.ok) throw new Error("Error with fetch hero-section");
-      return res.json();
-    })
-    .catch((e) => console.error(e));
-
-  const aboutData = await fetch(
-    "http://localhost:1337/api/about-section?populate=*",
-    {
-      next: { revalidate: 60 },
-    }
-  )
-    .then((res) => {
-      if (!res.ok) throw new Error("Error with fetch about-section");
-      return res.json();
-    })
-    .catch((e) => console.error(e));
-
   //GET AnimalsDB
-  const animalsDB = await fetch(
-    "http://localhost:1337/api/dogs?populate=img&populate=moreImg",
-    {
-      next: { revalidate: 60 },
-    }
-  )
-    .then((res) => {
-      if (!res.ok) throw new Error("Error with get data from animalsDB");
-      return res.json();
-    })
-    .catch((e) => console.error(e));
+  const animalsDB = await fetchApi("/dogs?populate=img&populate=moreImg");
 
   //CREATE DB ONLY FOR CATS
   const catsData = animalsDB.data
     .filter((item) => item.category != "dogs")
     .slice(0, 3);
 
+  //GET DATA FOR HERO SECTION FROM API
+  const heroData = await fetchApi("/hero-section");
   const dataHeroSection = {
     pageTitle: heroData.data?.pageTitle || "",
     title: heroData.data?.title || "",
     description: heroData.data?.description || "",
   };
 
-  // Get DATA for SearchSection from API
-  const searchData = await fetch("http://localhost:1337/api/search-section", {
-    next: { revalidate: 60 },
-  })
-    .then((res) => {
-      if (!res.ok) throw new Error("Error with searc-section");
-      return res.json();
-    })
-    .catch((e) => console.error(e));
-
+  //GET DATA FOR SEARCH SECTION FROM API
+  const searchData = await fetchApi("/search-section");
   const dataSearchSection = {
     title: searchData.data?.title,
     btnAbout: {
@@ -85,7 +49,8 @@ export default async function HomePage() {
     items: animalsDB,
   };
 
-  // GET data for About from API
+  //GET DATA FOR ABOUT SECTION FROM API
+  const aboutData = await fetchApi("/about-section?populate=*");
   const dataAboutSection = {
     title: aboutData.data?.title || "",
     description: aboutData.data?.description || "",
@@ -103,19 +68,10 @@ export default async function HomePage() {
     })),
   };
 
-  // TO DO: get DATA for OurAnimals from API
-  const ourAnimalsData = await fetch(
-    "http://localhost:1337/api/our-animal?populate=dogs&populate=cats",
-    {
-      next: { revalidate: 60 },
-    }
-  )
-    .then((res) => {
-      if (!res.ok) throw new Error("Failed with animals data");
-      return res.json();
-    })
-    .catch((e) => console.error(e));
-
+  //GET DATA FOR ANIMALS LIST FROM API
+  const ourAnimalsData = await fetchApi(
+    "/our-animal?populate=dogs&populate=cats"
+  );
   const dataOurAnimals = [
     {
       slug: ourAnimalsData.data?.cats[0]?.slug || "",
@@ -131,23 +87,12 @@ export default async function HomePage() {
     },
   ];
 
-  // Get data for helpSectionFrom API
-  const helpData = await fetch(
-    "http://localhost:1337/api/help-section?populate=*",
-    {
-      next: { revalidate: 60 },
-    }
-  )
-    .then((res) => {
-      if (!res.ok) throw new Error("Error with help-data");
-      return res.json();
-    })
-    .catch((e) => console.error(e));
-
+  //GET DATA FOR HELP SECTION FROM API
+  const helpData = await fetchApi("/help-section?populate=helpListItem");
   const dataHelpSection = {
     title: helpData.data?.title || "",
     description: helpData.data?.description || "",
-    items: helpData.data.HelpListItem.map((item) => item),
+    items: helpData.data.helpListItem.map((item) => item),
     btn: { title: t("buttons.more"), href: "/help" },
   };
 
