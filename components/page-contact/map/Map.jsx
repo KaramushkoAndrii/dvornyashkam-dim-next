7f22e2d13748b66e9ee87f94bb420311b7fa2cdb
Andii Karamushko <karamushko1997@gmail.com>
2025-11-16 22:03:27 +0200

    ref: add current link for google maps and changed styles for link in leaflet

--- components/page-contact/map/Map.jsx
+++ components/page-contact/map/Map.jsx
@@ -1,48 +1,19 @@
 "use client";
 
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-// // import "leaflet/dist/leaflet.css";
-// import "./map.scss";
-// import L from "leaflet";
-// import { useState } from "react";
-
-// delete L.Icon.Default.prototype._getIconUrl;
-
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl: "/leaflet/marker-icon-2x.png",
-//   iconUrl: "/leaflet/marker-icon.png",
-//   shadowUrl: "/leaflet/marker-shadow.png",
-// });
-
-// const Map = ({ position }) => {
-//   return (
-//     <div className="map__wrapper">
-//       <MapContainer
-//         center={position}
-//         zoom={17}
-//         style={{ width: "100vw", height: "100vh" }}
-//         scrollWheelZoom={false}
-//       >
-//         <TileLayer
-//           // Открытый источник карт
-//           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         />
-//         <Marker position={position}>
-//           <Popup>"ПРиют находится здесь"</Popup>
-//         </Marker>
-//       </MapContainer>
-//     </div>
-//   );
-// };
-
-// export default Map;`
-
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Icon } from "leaflet";
 import "./map.scss";
 
 const Map = ({ mapData }) => {
+  if (!mapData) {
+    return (
+      <div
+        className="map__wrapper"
+        style={{ height: "400px", background: "#eee" }}
+      />
+    );
+  }
+
   const {
     latitude,
     longitude,
@@ -55,14 +26,16 @@ const Map = ({ mapData }) => {
   } = mapData;
 
   const markerSize = [markerWidth, markerHeight];
+
   const newIcon = new Icon({
     iconUrl: markerIcon.url,
     iconSize: markerSize,
   });
 
   const positionMarker = [latitude, longitude];
+
   return (
-    <MapContainer center={[48.8566, 2.3522]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={positionMarker} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -71,7 +44,14 @@ const Map = ({ mapData }) => {
         <Popup>
           <h2>{popupText}</h2>
           <br />
-          <a href={googleMapsLink}>{linkText}</a>
+          <a
+            className="map__link"
+            href={`https://www.google.com/maps?q=${latitude},${longitude}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {linkText}
+          </a>
         </Popup>
       </Marker>
     </MapContainer>
