97a4e9fdaa24e1c5b527b31ec5ab1322f8af5a36
Andii Karamushko <karamushko1997@gmail.com>
2025-11-23 21:29:08 +0200

    ref: changed size of slide in slider and changed styles in AnimalDetailds.scss

--- app/[locale]/ivents/page.jsx
+++ app/[locale]/ivents/page.jsx
@@ -1,5 +1,10 @@
 import "./page.scss";
 
 export default async function iventsPage() {
-  return <h1>Hi!</h1>;
+  return (
+    <h1>
+      На цій сторінці ви зможете ознайомитись з усіма івентами які ми робимо для
+      того, аби наповнити життя наших тваринок радістю
+    </h1>
+  );
 }
