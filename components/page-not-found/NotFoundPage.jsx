9b26315e08d472e9af994a9c44c9d34b10be3afd
Andii Karamushko <karamushko1997@gmail.com>
2025-11-28 00:01:47 +0200

    ref: changed styles in NotFoundPage

--- components/page-not-found/NotFoundPage.jsx
+++ components/page-not-found/NotFoundPage.jsx
@@ -1,16 +1,33 @@
 import { useTranslations } from "next-intl";
+import Image from "next/image";
+import Button from "../UI/button/Button";
+import { fetchApi } from "@/lib/api";
 
 import "./NotFoundPage.scss";
 
 export default function NotFoundPage() {
-  // TO DO: Need some styles
   const t = useTranslations("NotFound");
 
   return (
     <div className="not-found-page">
-      <h1 className="not-found-page__title title">
-        <b>404</b> - {t("title")}
-      </h1>
+      <div className="not-found-page__aside">
+        <div className="not-found-page__content">
+          <h2 className="not-found-page__title h2">
+            <b>Ой, мы потерялись</b>
+          </h2>
+          <p className="not-found-page__description p">
+            Страница которую вы ищите. не существует, но вы можете найти других
+            животных в нашем приюте
+          </p>
+        </div>
+        <div className="not-found-page__buttons">
+          <Button text={t("animals")} href="/animals" variant="to-animals" />
+          <Button text={t("home")} href="/" variant="to-home" />
+        </div>
+      </div>
+      <div className="not-found-page__image">
+        <Image src="/images/happy2.png" fill={true} />
+      </div>
     </div>
   );
 }
