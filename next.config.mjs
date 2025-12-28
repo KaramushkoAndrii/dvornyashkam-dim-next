fa862f050c846e6c1c15ace9d85ddbc7071d80a1
Andii Karamushko <karamushko1997@gmail.com>
2025-12-28 23:48:35 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -32,6 +32,47 @@
 
 // export default withNextIntl(nextConfig);
 
+// import createNextIntlPlugin from "next-intl/plugin";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const withNextIntl = createNextIntlPlugin();
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "http",
+//         hostname: "localhost",
+//         port: "1337",
+//         pathname: "/uploads/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "res.cloudinary.com",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+
+//   sassOptions: {
+//     includePaths: [path.join(__dirname), path.join(__dirname, "src")],
+
+//     additionalData: `
+//       @use "assets/styles/vars.scss" as *;
+//       @use "assets/styles/mixins.scss" as *;
+//     `,
+
+//     silenceDeprecations: ["legacy-js-api"],
+//   },
+// };
+
+// export default withNextIntl(nextConfig);
+
 import createNextIntlPlugin from "next-intl/plugin";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -51,22 +92,15 @@ const nextConfig = {
         port: "1337",
         pathname: "/uploads/**",
       },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "/**",
-      },
+      { protocol: "https", hostname: "res.cloudinary.com", pathname: "/**" },
     ],
   },
-
   sassOptions: {
     includePaths: [path.join(__dirname), path.join(__dirname, "src")],
-
     additionalData: `
-      @use "assets/styles/vars.scss" as *;
-      @use "assets/styles/mixins.scss" as *;
+      @use "@/assets/styles/vars.scss" as *;
+      @use "@/assets/styles/mixins.scss" as *;
     `,
-
     silenceDeprecations: ["legacy-js-api"],
   },
 };
